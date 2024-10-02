9e8de1170d8db4ce2c5a7ea3fcbac3f5ddfebc92
Alex Gordon <alexg1991@outlook.com>
2024-10-02 15:28:41 +0000

    Using imported flash function, provide feedback to the user upon form completion

--- run.py
+++ run.py
@@ -1,9 +1,13 @@
 import os
 import json
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, flash
+#imports - render_templates to clean up html pages, request for the form element
+#imports - flash to show temp messages to user
+if os.path.exists("env.py"):
+    import env
 
 app = Flask(__name__)
+app.secret_key = os.environ.get("SECRET_KEY")
 
 
 @app.route("/")
@@ -32,8 +36,7 @@ def about_member(member_name):
 @app.route("/contact", methods=["GET", "POST"])
 def contact():
     if request.method == "POST":
-        print(request.form.get("name"))
-        print(request.form["email"])
+        flash("Thanks {}, we have received your message!" .format(request.form.get("name")))
     return render_template("contact.html", page_title="Contact")
 
 
